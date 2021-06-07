802c5d65b0a03c9d58ad2884f44d737ea50b4784
Rameshkumar R <rramesh.rsd@gmail.com>
2021-06-07 19:02:55 +0530

    First Commit
    First Commit for MyDemoProject

---
+++ src/main/java/com/devops2/App.java
@@ -0,0 +1,14 @@
+package com.devops2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey There!" );
+       
+    }
+}
